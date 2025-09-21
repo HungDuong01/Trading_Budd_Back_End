39ef580e81323dcfcc0d618393d86eb2676194fc
HungDuong01 <hungdc257@gmail.com>
2025-09-21 15:45:03 +0700

    Inital Commit

---
+++ src/main/java/com/example/Trading_Budd/TradingBuddApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trading_Budd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingBuddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingBuddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Trading_Budd/TradingBuddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trading_Budd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBuddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
